47dc76ec9564c14287302760fe9a89ae513a0935
Matheus <37225059+blackcrw@users.noreply.github.com>
2024-03-06 00:24:33 -0300

    feat(#4): create color printers

---
+++ color.go
@@ -0,0 +1,16 @@
+package main
+
+const (
+	RED       = "\u001b[31;1m"
+	BLUE      = "\u001b[34;1m"
+	GREEN     = "\u001b[32;1m"
+	BLACK     = "\u001b[30;1m"
+	WHITE     = "\u001b[37;1m"
+	YELLOW    = "\u001b[33;1m"
+	MAGENTA   = "\u001b[35;1m"
+	CYAN      = "\u001b[36;1m"
+	RESET     = "\u001b[0m"
+	BOLD      = "\u001b[1m"
+	UNDERLINE = "\u001b[4m"
+	REVERSED  = "\u001b[7m"
+)
---
+++ printer.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+	"syscall"
+)
+
+const (
+	DONE    = GREEN + "[DONE]" + RESET
+	DANGER  = RED + "[DANGER]" + RESET
+	FATAL   = RED + "[ERROR]" + RESET
+	INFO    = MAGENTA + "[INFO]" + RESET
+	WARNING = YELLOW + "[WARNING]" + RESET
+	SCAN    = YELLOW + "[QUESTION]" + RESET
+	VERBOSE = BLUE + "[VERBOSE]" + RESET
+)
+
+var (
+	stdin  = *os.Stdin
+	stdout = *os.Stdout
+	stderr = *os.Stderr
+)
+
+func Println(a ...interface{}) {
+	stdout.WriteString(doPrintbs(a...) + "\n")
+}
+
+func Printf(format string, a ...interface{}) {
+	stdout.WriteString(fmt.Sprintf(format, a...))
+}
+
+func Print(a ...interface{}) {
+	stdout.WriteString(doPrintbs(a...))
+}
+
+func Fatal(a ...interface{}) {
+	stderr.WriteString(doPrintbs(a...))
+	syscall.Exit(1)
+}
+
+func Done(t ...interface{}) {
+	stdout.WriteString(DONE + " " + doPrintbs(t...) + "\n")
+}
+
+func Danger(t ...interface{}) {
+	stdout.WriteString(DANGER + " " + doPrintbs(t...) + "\n")
+}
+
+func Warning(t ...interface{}) {
+	stdout.WriteString(WARNING + " " + doPrintbs(t...) + "\n")
+}
+
+func Info(t ...interface{}) {
+	stdout.WriteString(INFO + " " + doPrintbs(t...) + "\n")
+}
+
+func Verbose(t ...interface{}) {
+	stdout.WriteString(VERBOSE + " " + doPrintbs(t...) + "\n")
+}
+
+func ScanQ(t ...interface{}) (string, error) {
+	stdout.WriteString(SCAN + " " + doPrintbs(t...))
+
+	var response, err = bufio.NewReader(&stdin).ReadString('\n')
+
+	if err != nil {
+		return "", err
+	}
+
+	response = strings.ToLower(response)
+
+	if response == "\n" {
+		return response, nil
+	}
+
+	response = strings.Replace(response, "\n", "", -1)
+
+	return response, nil
+}
