23887f8feb624223bb8557e81cc48cd90a645d0d
Matheus <37225059+blackcrw@users.noreply.github.com>
2024-03-06 20:48:23 -0300

    feat(#4): create color printers

---
+++ concat.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func doPrintbs(a ...interface{}) (str string) {
+	for count, arg := range a {
+		if count > 0 {
+			str += " "
+		}
+
+		switch arg.(type) {
+		case bool:
+			str += fmt.Sprint(arg.(bool))
+		case string:
+			str += arg.(string)
+		case error:
+			str += arg.(error).Error()
+		case int:
+			str += fmt.Sprint(arg.(int))
+		case float64:
+			str += fmt.Sprint(arg.(float64))
+		case float32:
+			str += fmt.Sprint(arg.(float32))
+		}
+	}
+
+	return
+}
--- global.go
+++ global.go
@@ -14,6 +14,31 @@ const (
 	VERBOSE  = "[VERBOSE]"
 )
 
+const (
+	RED       = "\u001b[31;1m"
+	BLUE      = "\u001b[34;1m"
+	GREEN     = "\u001b[32;1m"
+	BLACK     = "\u001b[30;1m"
+	WHITE     = "\u001b[37;1m"
+	YELLOW    = "\u001b[33;1m"
+	MAGENTA   = "\u001b[35;1m"
+	CYAN      = "\u001b[36;1m"
+	RESET     = "\u001b[0m"
+	BOLD      = "\u001b[1m"
+	UNDERLINE = "\u001b[4m"
+	REVERSED  = "\u001b[7m"
+)
+
+const (
+	COLORDONE     = GREEN + "[DONE]" + RESET
+	COLORDANGER   = RED + "[DANGER]" + RESET
+	COLORFATAL    = RED + "[ERROR]" + RESET
+	COLORINFO     = MAGENTA + "[INFO]" + RESET
+	COLORWARNING  = YELLOW + "[WARNING]" + RESET
+	COLORQUESTION = YELLOW + "[QUESTION]" + RESET
+	COLORVERBOSE  = BLUE + "[VERBOSE]" + RESET
+)
+
 var (
 	stdin  = *os.Stdin
 	stdout = *os.Stdout
